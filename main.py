d4177773a05470f43513b6a9e3e8c14847d2d6c4
Attreya <attreya01@gmail.com>
2020-02-21 16:26:03 +0530

    NO NLTK. Shows emotions of text and displays them using in a graph with Matplotlib

---
+++ main.py
@@ -0,0 +1,45 @@
+import string
+from collections import Counter
+import matplotlib.pyplot as plt
+
+# reading text file
+text = open("read.txt", encoding="utf-8").read()
+
+# converting to lowercase
+lower_case = text.lower()
+
+# Removing punctuations
+cleaned_text = lower_case.translate(str.maketrans('', '', string.punctuation))
+
+# splitting text into words
+tokenized_words = cleaned_text.split()
+
+stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
+              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
+              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
+              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
+              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
+              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
+              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+
+# Removing stop words from the tokenized words list
+final_words = [word for word in tokenized_words if word not in stop_words]
+
+# Get emotions text
+emotion_list = []
+with open('emotions.txt', 'r') as file:
+    for line in file:
+        clear_line = line.replace('\n', '').replace(',', '').replace("'", '').strip()
+        word, emotion = clear_line.split(':')
+        if word in final_words:
+            emotion_list.append(emotion)
+
+w = Counter(emotion_list)
+print(w)
+plt.bar (w.keys(), w.values())
+
+plt.savefig('graph.png')
+plt.show()
