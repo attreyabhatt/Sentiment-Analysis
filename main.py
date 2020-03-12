a1bd346c3b7a0304f4fecb2a7387cb45f540eef0
Attreya <attreya01@gmail.com>
2020-03-12 18:23:53 +0530

    NO NLTK. Shows emotions of text and displays them using in a graph with Matplotlib Inn this Commit added NLTK and improved matplotlib graph

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import string
 from collections import Counter
+
 import matplotlib.pyplot as plt
 
 # reading text file
@@ -26,20 +27,37 @@ stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you"
               "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
 
 # Removing stop words from the tokenized words list
-final_words = [word for word in tokenized_words if word not in stop_words]
+final_words = []
+for word in tokenized_words:
+    if word not in stop_words:
+        final_words.append(word)
 
-# Get emotions text
+# NLP Emotion Algorithm
+# 1) Check if the word in the final word list is also present in emotion.txt
+#  - open the emotion file
+#  - Loop through each line and clear it
+#  - Extract the word and emotion using split
+
+# 2) If word is present -> Add the emotion to emotion_list
+# 3) Finally count each emotion in the emotion list
+
 emotion_list = []
 with open('emotions.txt', 'r') as file:
     for line in file:
-        clear_line = line.replace('\n', '').replace(',', '').replace("'", '').strip()
+        clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
         word, emotion = clear_line.split(':')
+
         if word in final_words:
             emotion_list.append(emotion)
 
+print(emotion_list)
 w = Counter(emotion_list)
 print(w)
-plt.bar (w.keys(), w.values())
 
+# Plotting the emotions on the graph
+
+fig, ax1 = plt.subplots()
+ax1.bar(w.keys(), w.values())
+fig.autofmt_xdate()
 plt.savefig('graph.png')
 plt.show()
---
+++ main_nltk.py
@@ -0,0 +1,59 @@
+import string
+from collections import Counter
+
+import matplotlib.pyplot as plt
+from nltk.corpus import stopwords
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+text = open('read.txt', encoding='utf-8').read()
+lower_case = text.lower()
+cleaned_text = lower_case.translate(str.maketrans('', '', string.punctuation))
+
+# Using word_tokenize because it's faster than split()
+tokenized_words = word_tokenize(cleaned_text, "english")
+
+# Removing Stop Words
+final_words = []
+for word in tokenized_words:
+    if word not in stopwords.words('english'):
+        final_words.append(word)
+
+# Lemmatization - From plural to single + Base form of a word (example better-> good)
+lemma_words = []
+for word in final_words:
+    word = WordNetLemmatizer().lemmatize(word)
+    lemma_words.append(word)
+
+emotion_list = []
+with open('emotions.txt', 'r') as file:
+    for line in file:
+        clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
+        word, emotion = clear_line.split(':')
+
+        if word in lemma_words:
+            emotion_list.append(emotion)
+
+print(emotion_list)
+w = Counter(emotion_list)
+print(w)
+
+
+def sentiment_analyse(sentiment_text):
+    score = SentimentIntensityAnalyzer().polarity_scores(sentiment_text)
+    if score['neg'] > score['pos']:
+        print("Negative Sentiment")
+    elif score['neg'] < score['pos']:
+        print("Positive Sentiment")
+    else:
+        print("Neutral Sentiment")
+
+
+sentiment_analyse(cleaned_text)
+
+fig, ax1 = plt.subplots()
+ax1.bar(w.keys(), w.values())
+fig.autofmt_xdate()
+plt.savefig('graph.png')
+plt.show()
